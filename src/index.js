c053c005464bd2d12035855b91838a420a45325f
Alex Hoyau <a.hoyau@silexlabs.org>
2014-12-09 17:08:29 +0100

    added auto load (url param)

--- src/index.js
+++ src/index.js
@@ -9,5 +9,20 @@ goog.require('rsz.Responsizer');
 window['initResponsizeApp'] = function initResponsizeApp() {
   var appElement = document.getElementById('rsz-app');
   var app = new App(appElement);
+  // debug:
+  var query = getQueryParams(document.location.search);
+  if (query.url) {
+    app.onOpen({'url': query.url});
+  }
+}
+
+
+function getQueryParams(qs) {
+  qs = qs.split("+").join(" ");
+  var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g;
+  while (tokens = re.exec(qs)) {
+    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+  }
+  return params;
 }
 
